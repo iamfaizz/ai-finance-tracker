c02dc116ef75ffa4d0ed46cb6636ae2a1227a091
Faiz Ahmad Khan <iamfaiz@Faizzs-MacBook-Air.local>
2026-06-14 22:26:50 +0530

    Clean db connection

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: 'db.rlqgojouqfcpkivgywgz.supabase.co',
-  port: 5432,
-  database: 'postgres',
-  user: 'postgres',
-  password: 'Faizkhan0112',
-  ssl: { rejectUnauthorized: false },
-  family: 4
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
